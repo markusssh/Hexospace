734c0b0a3dc27ea0d4f8f09a192117941b526569
Mark Semakin <st099590@student.spbu.ru>
2024-03-12 23:04:01 +0300

    working on generator

--- game/tile_handler.gd
+++ game/tile_handler.gd
@@ -15,3 +15,27 @@ func get_pointed_coord() -> Vector2i:
 	var pointed_coord = mouse_pos + view_pos
 	var hex_pointed_at = world_map.local_to_map(pointed_coord)
 	return hex_pointed_at
+	
+	
+func get_ring(cell : Vector2i, rad : int = 1) -> Array:
+	var res : Array
+	var grid_size : int = 2 * rad + 1
+	var center_coord : int = (grid_size - 1) / 2
+	var center : Vector2i = Vector2i(center_coord, center_coord)
+	for i in range(grid_size):
+		for j in range(grid_size):
+			var local_cell : Vector2i = Vector2i(i, j)
+			var on_h_border = abs(grid_size - 1 - local_cell.x) == grid_size - 1
+			var on_v_border = abs(grid_size - 1 - local_cell.y) == grid_size - 1
+			var is_border_local_cell : bool = on_h_border or on_v_border
+			var is_corner_local_cell : bool = abs(local_cell - center) != center
+			if is_border_local_cell and !is_corner_local_cell:
+				pass
+	return []
+
+
+func _cell_exists(cell : Vector2i) -> bool:
+	var coord_max = world_map.COORD_MAX
+	var not_below_bottom_range = cell.x >= 0 and cell.y >= 0
+	var not_above_top_range = cell.x <= coord_max.x and cell.y <= coord_max.y
+	return not_below_bottom_range and not_above_top_range
--- game/world_map.gd
+++ game/world_map.gd
@@ -1,12 +1,13 @@
 extends TileMap
 
 enum TERRAIN_TYPE {
-	WATER = 0,
+	COAST = 0,
 	GRASSLAND = 1,
 	PLAINS = 2,
 	DESERT = 3,
 	TUNDRA = 4,
-	SNOWLAND = 5
+	SNOWLAND = 5,
+	OCEAN = 6
 }
 enum SUBTERRAIN_TYPE {
 	MOUNTAIN = 0
@@ -16,12 +17,13 @@ enum UNIT_TYPE {
 }
 
 const TERRAIN_ATLAS_COORD = {
-	TERRAIN_TYPE.WATER: Vector2i(0, 0),
+	TERRAIN_TYPE.COAST: Vector2i(0, 0),
 	TERRAIN_TYPE.GRASSLAND: Vector2i(1, 0),
 	TERRAIN_TYPE.PLAINS: Vector2i(0, 1),
 	TERRAIN_TYPE.DESERT: Vector2i(1, 1),
 	TERRAIN_TYPE.TUNDRA: Vector2i(0, 2),
 	TERRAIN_TYPE.SNOWLAND: Vector2i(1, 2),
+	TERRAIN_TYPE.OCEAN: Vector2i(2, 0)
 }
 const UNIT_ATLAS_COORD = {
 	UNIT_TYPE.SETTLER: Vector2i(0, 0)
@@ -30,10 +32,11 @@ const COORD_MAX = Vector2i(100, 100)
 const TILE_PIXEL_SIZE = 128
 
 const WATER_LEVEL = 0.01
+const OCEAN_LEVEL = -0.15
 const MOUNTAIN_LEVEL = 0.5
 
-const DESERT_TEMPERATURE = 0.6
-const PLAINS_TEMPERATURE = 0.2
+const DESERT_TEMPERATURE = 0.4
+const PLAINS_TEMPERATURE = 0.19
 const GRASSLAND_TEMPERATURE = -0.1
 const TUNDRA_TEMPERATURE = -0.4
 
@@ -57,11 +60,11 @@ func _ready():
 	set_layer_name(_terrain_grid, "Terrain")
 	
 	add_layer(_subterrain_grid)
-	set_layer_enabled(_subterrain_grid, true)
+	set_layer_enabled(_subterrain_grid, false)
 	set_layer_name(_subterrain_grid, "Subterrain")
 	
 	add_layer(_hex_grid)
-	set_layer_enabled(_hex_grid, false)
+	set_layer_enabled(_hex_grid, true)
 	set_layer_name(_hex_grid, "Hex Grid")
 	
 	add_layer(_unit_grid)
@@ -71,63 +74,12 @@ func _ready():
 	var seed = randi()
 	_altitude.seed = seed
 	_temperature.seed = seed
-	
 	_generate_map()
+	_smooth_map()
 	_generate_init_settler()
 	
-func _process(delta) -> void:
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		_set_grassland_tile(tile_handler.get_pointed_coord())
-	elif Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
-		_set_water_tile(tile_handler.get_pointed_coord())
-	
-	
-func _set_grassland_tile(coord : Vector2i) -> void:
-	set_cell(
-		_terrain_grid,
-		coord,
-		_terrain_grid,
-		TERRAIN_ATLAS_COORD[TERRAIN_TYPE.GRASSLAND]
-	)
-	
-	
-func _set_water_tile(coord : Vector2i) -> void:
-	set_cell(
-		_terrain_grid,
-		coord,
-		_terrain_grid,
-		TERRAIN_ATLAS_COORD[TERRAIN_TYPE.WATER]
-	)
-	
-	
-func _add_unit(unit_type : UNIT_TYPE, coord : Vector2i):
-	match unit_type:
-			UNIT_TYPE.SETTLER:
-				set_cell(
-					_unit_grid,
-				 	coord,
-				 	_unit_grid,
-				 	UNIT_ATLAS_COORD[UNIT_TYPE.SETTLER]
-				)
-	
 
-#region WorldGen
-func _generate_init_settler() -> void:
-	var coord = _get_starting_spot()
-	_add_unit(UNIT_TYPE.SETTLER, coord)
-	starting_coord = map_to_local(coord)
-
-
-func _get_starting_spot() -> Vector2i:
-	var coord : Vector2i = get_used_cells(_terrain_grid).pick_random()
-	while get_cell_tile_data(
-		_terrain_grid,
-		coord
-	).get_custom_data("type") == TERRAIN_TYPE.WATER:
-		coord = get_used_cells(_terrain_grid).pick_random()
-	return coord
-	
-
+#region Initial_Generation
 func _generate_map() -> void:
 	for i in range(COORD_MAX.x):
 		for j in range(COORD_MAX.y):
@@ -138,6 +90,7 @@ func _generate_map() -> void:
 				 _terrain_grid,
 				 terrain_texture_coord
 			)
+			#grid outline
 			set_cell(
 				_hex_grid,
 				 Vector2i(i, j),
@@ -158,7 +111,10 @@ func _get_hex_type(cell : Vector2i) -> TERRAIN_TYPE:
 	var alt = _get_altitude_by_cell(cell)
 	var temp = _get_temperature_by_cell(cell)
 	if alt <= WATER_LEVEL:
-		return TERRAIN_TYPE.WATER
+		if alt <= OCEAN_LEVEL:
+			return TERRAIN_TYPE.OCEAN
+		else:
+			return TERRAIN_TYPE.COAST
 	else:
 		temp = _normalize_temperature(temp, cell)
 		if temp >= DESERT_TEMPERATURE:
@@ -180,27 +136,34 @@ func _normalize_temperature(temp: float, cell : Vector2i) -> float:
 	
 
 func _normalize_cold_temerature(temp: float, cell : Vector2i) -> float:
+	var border_offset : int = min(abs(COORD_MAX.y - cell.y), cell.y)
 	if _cell_is_in_cold_zone(cell):
-		var border_offset : int = min(abs(COORD_MAX.y - cell.y), cell.y)
-		temp = temp - 0.09 * (COLD_ZONE * 100 - border_offset)
-	else:
-		if temp < 0:
-			var border_offset : int = min(abs(COORD_MAX.y - cell.y), cell.y)
-			temp = max(0, temp + 0.02 * (border_offset - COLD_ZONE * 100))
+		temp = temp - 0.08 * (COLD_ZONE * COORD_MAX.y - border_offset)
+	elif temp < GRASSLAND_TEMPERATURE:
+		temp = min(GRASSLAND_TEMPERATURE,
+				temp + 0.03 * (border_offset - COLD_ZONE * COORD_MAX.y))
 	return temp
 	
 
 func _normalize_hot_temperature(temp: float, cell : Vector2i) -> float:
-	
+	var equator_offset = abs(COORD_MAX.y / 2 - cell.y)
+	if _cell_is_in_hot_zone(cell):
+		temp = temp + 0.02 * (HOT_ZONE * COORD_MAX.y - equator_offset / 2)
+	elif temp >= DESERT_TEMPERATURE:
+		temp = max(PLAINS_TEMPERATURE,
+				temp - 0.02 * (equator_offset - COLD_ZONE * COORD_MAX.y))
+	return temp
 
 
 func _cell_is_in_hot_zone(cell : Vector2i) -> bool:
-	
+	var is_above_normal_zone : bool = cell.y >= (COORD_MAX.y - HOT_ZONE * COORD_MAX.y) / 2
+	var is_below_normal_zone : bool = cell.y <= (COORD_MAX.y + HOT_ZONE * COORD_MAX.y) / 2
+	return is_above_normal_zone and is_below_normal_zone
 	
 
 func _cell_is_in_cold_zone(cell : Vector2i) -> bool:
-	var is_in_top_zone = cell.y <= COORD_MAX.y * COLD_ZONE
-	var is_in_bottom_zone = cell.y >= COORD_MAX.y * (1 - COLD_ZONE)
+	var is_in_top_zone : bool = cell.y <= COORD_MAX.y * COLD_ZONE
+	var is_in_bottom_zone : bool = cell.y >= COORD_MAX.y * (1 - COLD_ZONE)
 	return is_in_top_zone or is_in_bottom_zone
 
 
@@ -210,5 +173,47 @@ func _get_altitude_by_cell(cell : Vector2i) -> float:
 
 func _get_temperature_by_cell(cell : Vector2i) -> float:
 	return _temperature.get_noise_2d((cell.x + 100) * 5, (cell.y + 100) * 5)
+#endregion
+
+#region Smoothing
+func _smooth_map():
+	_adjust_coastline()
+	
+
+func _adjust_coastline():
+	for i in range(COORD_MAX.x):
+		for j in range(COORD_MAX.y):
+			if get_cell_atlas_coords(
+				_terrain_grid,
+				 Vector2i(i, j),
+			) == TERRAIN_ATLAS_COORD[TERRAIN_TYPE.OCEAN]:
+				var rad_1 : Array
+				
 
 #endregion
+
+func _generate_init_settler() -> void:
+	var coord = _get_starting_spot()
+	_add_unit(UNIT_TYPE.SETTLER, coord)
+	starting_coord = map_to_local(coord)
+
+
+func _get_starting_spot() -> Vector2i:
+	var coord : Vector2i = get_used_cells(_terrain_grid).pick_random()
+	while get_cell_tile_data(
+		_terrain_grid,
+		coord
+	).get_custom_data("is_water"):
+		coord = get_used_cells(_terrain_grid).pick_random()
+	return coord
+	
+	
+func _add_unit(unit_type : UNIT_TYPE, coord : Vector2i):
+	match unit_type:
+			UNIT_TYPE.SETTLER:
+				set_cell(
+					_unit_grid,
+				 	coord,
+				 	_unit_grid,
+				 	UNIT_ATLAS_COORD[UNIT_TYPE.SETTLER]
+				)
